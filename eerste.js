c56f13320d1fb61ccff53ef5e4e9c35def2b371a
Vikkeyeftimov <Vikksaus@hotmail.com>
2023-12-18 13:31:26 +0100

    ookr ijfds

--- eerste.js
+++ eerste.js
@@ -1,16 +1,16 @@
 let button = document.querySelector("#weerver");
 let slide = document.querySelector(".slide");
 let sunny = document.querySelector(".sunny");
-// let mooiweer = document.querySelector(".mooiweer")
+
 
 button.addEventListener('click', veranderen)
 function veranderen(){
     sunny.classList.toggle("mooiweer")
-    slide.classList.toggle("mooiweer")
+    slide.classList.toggle("mooiweer") 
+    if(this.innerHTML === 'Zet zonnetje aan!') {
+        this.innerHTML = 'Laat het regenen!'
+    }  else {
+        this.innerHTML = 'Zet zonnetje aan!' 
+    }
 }
 
-// console.log(slide);
-
-// button.addEventListener("cl", function(){
-//     Element.classlist.toggle("sunny")
-// })
